cd574b5e6daf6e4a47adb17626595847be377dc2
Leonard de Ruijter <alderuijter@gmail.com>
2026-05-28 15:13:02 +0200

    fix(addon): tighten SAPIence token filter to full path component

--- addon/synthDrivers/sapi5.py
+++ addon/synthDrivers/sapi5.py
@@ -10,4 +10,6 @@ _SAPIENCE_TOKEN_SUFFIX = "SAPIence"
 class SynthDriver(SynthDriver):  # type: ignore[no-redef]  # noqa: F405,F811
 	def _getVoiceTokens(self):
 		tokens = super()._getVoiceTokens()
-		return [tokens[i] for i in range(len(tokens)) if not tokens[i].Id.endswith(_SAPIENCE_TOKEN_SUFFIX)]
+		return [
+			tokens[i] for i in range(len(tokens)) if not tokens[i].Id.endswith("\\" + _SAPIENCE_TOKEN_SUFFIX)
+		]
